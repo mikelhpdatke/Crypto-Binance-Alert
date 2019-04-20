c55478bee8e433a19fbf0dc416bb8fa720a6dc06
mikelhpdatke <luongductuandat.psa@gmail.com>
2019-04-20 15:10:30 +0700

    1p

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const Binance = require('binance-api-node').default
-const client = Binance()
+const Binance = require("binance-api-node").default;
+const client = Binance();
 
-const TelegramBot = require('node-telegram-bot-api');
-chatId = '-1001448473548';
+const TelegramBot = require("node-telegram-bot-api");
+chatId = "-1001448473548";
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '631244145:AAHSxbn0R-FwHqynb7Yn_8Eqz9VzJhKbIDs';
+const token = "631244145:AAHSxbn0R-FwHqynb7Yn_8Eqz9VzJhKbIDs";
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 tickers = [
-  'ETHUSDT',
-  'BTCUSDT',
-  'BNBUSDT',
-  'EOSUSDT',
-  'ETCUSDT',
-  'FETUSDT',
-  'HOTUSDT',
-  'NEOUSDT',
-  'LTCUSDT',
-  'ONTUSDT',
-  'TRXUSDT',
-  'XLMUSDT',
-  'XRPUSDT',
-]
+  "ETHUSDT",
+  "BTCUSDT",
+  "BNBUSDT",
+  "EOSUSDT",
+  "ETCUSDT",
+  "FETUSDT",
+  "HOTUSDT",
+  "NEOUSDT",
+  "LTCUSDT",
+  "ONTUSDT",
+  "TRXUSDT",
+  "XLMUSDT",
+  "XRPUSDT"
+];
 // Matches "/echo [whatever]"
 // bot.onText(/\/echo (.+)/, (msg, match) => {
 //   // 'msg' is the received Message from Telegram
@@ -49,26 +49,29 @@ tickers = [
 
 let record = new Map();
 
-function alertUp(symbol, prev, cur, percent){
+function alertUp(symbol, prev, cur, percent) {
   // console.log(prev, cur);
-  let flag = (prev < cur ? 'tăng': 'giảm');
+  let flag = prev < cur ? "tăng" : "giảm";
   // console.log(flag);
-  bot.sendMessage(chatId, `Phát hiện ${symbol} ${flag} ${percent}%
-${prev} \u{27A1} ${cur}`);
+  bot.sendMessage(
+    chatId,
+    `Phát hiện ${symbol} ${flag} ${percent}%
+${prev} \u{27A1} ${cur} trong 1 phút`
+  );
 }
 
-client.ws.candles(tickers, '15m', candle => {
-    if (!candle.isFinal) return;
-    let { symbol, close, volume } = candle;
-    let curPrice = record.get(symbol);
-    if (curPrice){
-      let percent = Math.abs((curPrice - close) / close) * 100;
+client.ws.candles(tickers, "1m", candle => {
+  if (!candle.isFinal) return;
+  let { symbol, close, volume } = candle;
+  let curPrice = record.get(symbol);
+  if (curPrice) {
+    let percent = Math.abs((curPrice - close) / close) * 100;
+    if (percent >= 2) {
       console.log(symbol, curPrice, close, percent);
-      if (percent >= 2){
-        alertUp(symbol, curPrice, close, percent);
-      }
+      alertUp(symbol, curPrice, close, percent);
     }
-    record.set(symbol, close);
+  }
+  record.set(symbol, close);
 });
 
 // alertUp('BTCUSDT', 123, 455, 6);
